fc4515d8e31fae11f6c4478f3aa8bd8751607b9c
gzocchi <z.giorgio.z@gmail.com>
2021-05-12 11:02:05 +0200

    modificato script Snack 3

--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -3,9 +3,10 @@
 // La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
 var numberArray = [26, 34, 8, 9, 10, 11, 12, 56, 85, 24, 75, 45, 1, 100, 3];
-console.log(numberArray);   // debug
+console.log(numberArray); // debug
 
-console.log(arrayInTheMiddle(numberArray, 3, 12));
+// console.log(arrayInTheMiddle(numberArray, 3, 12));
+console.log(estraiArray(numberArray, 2, 10));
 
 // FUNCTIONS
 function arrayInTheMiddle(array, a, b) {
@@ -17,4 +18,13 @@ function arrayInTheMiddle(array, a, b) {
     newArray.push(array[i]);
   }
   return newArray;
+}
+
+function estraiArray (array, a,b){
+  var newArray = [];
+  for (var i = a; i <= b; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+
 }
